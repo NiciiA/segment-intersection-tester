a67973f460fdacf2e236800875ccef573bb2458b
Nick <nicka99lp@gmail.com>
2025-04-15 12:26:34 +0200

    Create utils_cgal.h

---
+++ cpp/utils_cgal.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "utils.h"
+
+#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
+#include <CGAL/Arr_segment_traits_2.h>
+#include <CGAL/Surface_sweep_2_algorithms.h>
+#include <CGAL/intersections.h>
+#include <CGAL/Arrangement_2.h>
