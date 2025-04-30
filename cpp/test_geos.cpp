e5a999f3b22a07c285f0b0ace0e40c4b90c90143
Simon D Fink <finksim@fim.uni-passau.de>
2025-04-30 17:58:16 +0200

    fixed geos

--- cpp/test_geos.cpp
+++ cpp/test_geos.cpp
@@ -1,55 +1,56 @@
-#include <iostream>
-#include <vector>
-
+#include "utils.h"
+#include <geos/algorithm/LineIntersector.h>
 #include <geos/geom/Coordinate.h>
 #include <geos/geom/CoordinateSequence.h>
 #include <geos/noding/BasicSegmentString.h>
 #include <geos/noding/MCIndexNoder.h>
+#include <geos/noding/IntersectionFinderAdder.h>
+#include <geos/noding/NodingIntersectionFinder.h>
+#include <geos/noding/SimpleNoder.h>
 
 using namespace geos::geom;
 using namespace geos::noding;
+using namespace geos::algorithm;
 
-int main() {
-	std::vector<SegmentString*> segments;
+std::vector<SegmentString*> segments;
 
-	// Segment 1: (0, 0) -> (2, 2)
-	{
-		auto* coords = new CoordinateSequence();
-		coords->add(Coordinate(0, 0));
-		coords->add(Coordinate(2, 2));
-		segments.push_back(new BasicSegmentString(coords, nullptr));
-	}
-
-	// Segment 2: (0, 2) -> (2, 0)
-	{
-		auto* coords = new CoordinateSequence();
-		coords->add(Coordinate(0, 2));
-		coords->add(Coordinate(2, 0));
-		segments.push_back(new BasicSegmentString(coords, nullptr));
-	}
-
-	// Run the MCIndexNoder to find intersections
-	MCIndexNoder noder;
-	std::cout << "Calling computeNodes...\n";
-	noder.computeNodes(&segments);
-	std::cout << "Done.\n";
-
-	const auto* noded = noder.getNodedSubstrings();
-	for (const auto& seg : *noded) {
-		const CoordinateSequence* coords = seg->getCoordinates();
-		std::cout << "Segment: ";
-		for (std::size_t i = 0; i < coords->size(); ++i) {
-			auto c = coords->getAt(i);
-			std::cout << "(" << c.x << ", " << c.y << ") ";
-		}
-		std::cout << "\n";
-	}
-
-	// Cleanup
-	for (auto* seg : segments) {
-		delete seg->getCoordinates(); // since we manually allocated them
-		delete seg;
-	}
-
-	return 0;
+void process_line(const std::string& x1, const std::string& y1, const std::string& x2,
+		const std::string& y2) {
+	segments.push_back(new BasicSegmentString(nullptr,
+			new CoordinateSequence({Coordinate(bitstring_to_double(x1), bitstring_to_double(y1)),
+					Coordinate(bitstring_to_double(x2), bitstring_to_double(y2))})));
 }
+
+template<bool print>
+size_t compute_crossings() {
+	PrecisionModel pm(PrecisionModel::FLOATING);
+	LineIntersector li(&pm);
+	NodingIntersectionFinder ifa(li);
+	ifa.setFindAllIntersections(true);
+	SimpleNoder noder(&ifa);
+	noder.computeNodes(&segments);
+	return ifa.count() / 2;
+
+	// TODO test other noding strategies
+	// FIXME needed for print=true, but requires NodedSegmentStrings
+	// std::vector<Coordinate > intersections;
+	// IntersectionFinderAdder  ifa(li, intersections);
+	// const auto* noded = noder.getNodedSubstrings();
+	// for (const auto& seg : *noded) {
+	// 	const CoordinateSequence* coords = seg->getCoordinates();
+	// 	std::cout << "Segment: ";
+	// 	for (std::size_t i = 0; i < coords->size(); ++i) {
+	// 		auto c = coords->getAt(i);
+	// 		std::cout << "(" << c.x << ", " << c.y << ") ";
+	// 	}
+	// 	std::cout << "\n";
+	// }
+	//
+	// // Cleanup
+	// for (auto* seg : segments) {
+	// 	delete seg->getCoordinates(); // since we manually allocated them
+	// 	delete seg;
+	// }
+}
+
+#include "main.hpp"
