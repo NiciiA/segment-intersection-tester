07da8aa0b7157239ad69c0cf1291b468c52574f2
Manuel Sorge <manuel.sorge@pc191.ac.tuwien.ac.at>
2025-07-11 16:00:53 +0200

    add leda sweep intersection time generator

--- generation/leda/gen_leda.sh
+++ generation/leda/gen_leda.sh
@@ -16,6 +16,10 @@ if ! [ -f "gen_leda_sweep_time_difficult" ]; then
     echo "Please compile gen_leda_sweep_time_difficult first."
 fi
 
+if ! [ -f "gen_leda_geo_sweep_intersection_time" ]; then
+    echo "Please compile gen_leda_geo_sweep_intersection_time first."
+fi
+
 if ! [ -f "../number_converter.py" ]; then
     echo "Missing number converter in parent directory."
 fi
@@ -44,12 +48,15 @@ for d in {1..10}; do
     # Not needed: ../generator.py is already called with the corresponding arguments.
     # python3 gen_leda_highly_degenerate.py > testsets/leda_highly_degenerate_d${d}.csv
 
-    # Generate shifted-n-gon test set
-    for ((N=10; N<=10000; N*=100)); do
-	for ((r=800; r<=8000000; r*=100)); do
-	    ./gen_leda_circle_segments $N $r > testsets/leda_circle_segments_N${N}_r${r}.csv
-	    python3 ../number_converter.py --file testsets/leda_circle_segments_N${N}_r${r}.csv --output testsets-binary/leda_circle_segments_N${N}_r${r}.csv
-	done
-    done
+    # Generate "Sweep Intersection Time" test set as in the LEDA book: each instance with 2000 segments, each with one endpoint being a 30 bits random integer and the other the same integer with an added 5-bit random integer.
+    ./gen_leda_geo_sweep_intersection_time 2000 30 5 > testsets/leda_geo_sweep_intersection_time_d${d}_N2000_k30_s5.csv
+    python3 ../number_converter.py --file testsets/leda_geo_sweep_intersection_time_d${d}_N2000_k30_s5.csv --output testsets-binary/leda_geo_sweep_intersection_time_d${d}_N2000_k30_s5.csv
+done
 
+# Generate shifted-n-gon test set
+for ((N=10; N<=10000; N*=100)); do
+    for ((r=800; r<=8000000; r*=100)); do
+	./gen_leda_circle_segments $N $r > testsets/leda_circle_segments_N${N}_r${r}.csv
+	python3 ../number_converter.py --file testsets/leda_circle_segments_N${N}_r${r}.csv --output testsets-binary/leda_circle_segments_N${N}_r${r}.csv
+    done
 done
